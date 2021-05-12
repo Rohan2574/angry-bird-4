0d5dd5cdddb95d6316b06bb2556f9ade6dd0ac76
Rohan <rohanmetari7@gmail.com>
2021-05-12 13:48:13 +0530

    efg

--- sketch.js
+++ sketch.js
@@ -36,11 +36,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-    log6 = new Log(100,100,70,PI/2)
+    //log6 = new Log(100,100,70,PI/2)
 
-    bird = new Bird(100,100);
+    bird = new Bird(100,50);
 
-    slingshot=new SlingShot(bird.body,log6.body)
+    slingshot=new SlingShot(bird.body,{x:100,y:50})
 }
 
 function draw(){
@@ -63,10 +63,23 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-    log6.display();
+    //log6.display();
 
     bird.display();
     platform.display();
     slingshot.display();
     
+    
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+}
+function mouseReleased(){
+  slingshot.fly()  
+}
+function keyPressed(){
+   if(keyCode===32){
+      slingshot.attach(bird.body) 
+   }
 }
